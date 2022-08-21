1a8b81ca502292227ac9f568f582a21f1a89e196
Masha Rodionova <rodionovamv@gmai.com>
2022-08-21 23:29:23 +0300

    Corrected test

--- src/test/java/ru/netology/web/CardDeliveryPozitiveTest.java
+++ src/test/java/ru/netology/web/CardDeliveryPozitiveTest.java
@@ -3,7 +3,11 @@ package ru.netology.web;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.*;
@@ -29,6 +33,29 @@ class CardDeliveryPozitiveTest {
         Configuration.holdBrowserOpen = true;
         $("div [data-test-id='city'] input").setValue("Москва");
         $("div [data-test-id='date'] input").setValue("21.08.2022");
+        $("div [data-test-id='name'] input").setValue("Маша Родионова");
+        $("div [data-test-id='phone'] input").setValue("+79996663355");
+        $$("[data-test-id='agreement']").last().click();
+        $$(By.className("button__content")).last().click();
+        $("div.notification__title").shouldHave(exactText("Успешно!"), Duration.ofSeconds(40));
+    }
+
+    @Test
+    void shouldRegisterTest2() {
+        open("http://localhost:9999");
+        Configuration.holdBrowserOpen = true;
+
+        // задаем текущую дату ПЛЮС 3 дня
+        Date currentDate = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(currentDate);
+        c.add(Calendar.DATE, 3);
+        SimpleDateFormat formatForCurrentDate = new SimpleDateFormat("dd.MM.y");
+        String newCurrentDate = formatForCurrentDate.format(c.getTime());
+
+        //заполняем форму, проверяем
+        $("div [data-test-id='city'] input").setValue("Москва");
+        $("div [data-test-id='date'] input").setValue(newCurrentDate);
         $("div [data-test-id='name'] input").setValue("Маша Родионова");
         $("div [data-test-id='phone'] input").setValue("+79996663355");
         $$("[data-test-id='agreement']").last().click();
